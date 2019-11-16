fa6e1f21c476e076ce304f9b98001fa39161add1
Georg Meyer <georg@magneticbytes.io>
2019-11-16 15:20:04 +0100

    Add GA

---
+++ docs/analytics.js
@@ -0,0 +1,27 @@
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+let gaId = window.location.host.indexOf('happy-yoga-ka.de') !== -1 ? 'UA-152700864-1' : 'UA-152700864-2';
+
+ga('set', 'anonymizeIp', true);
+ga('create', gaId, {
+    'storage': 'none',
+    'clientId': uuidv4()
+});
+ga('send', 'pageview');
